f372bcfdb045c5cff180b6c6c1ea20e5373f6ce2
Inga Kononova <inga.kononova@gmail.com>
2024-03-27 11:47:44 +0200

    Code for the 5th task

---
+++ lesson-01/05.js
@@ -0,0 +1,36 @@
+let rule =
+  "Еще не родился тот человек, который поставил бы цель и не смог бы стать программистом.";
+
+switch (rule.length) {
+  case 1:
+  case 2:
+  case 3:
+  case 4:
+  case 5:
+  case 6:
+  case 7:
+  case 8:
+  case 9:
+  case 10:
+  case 11:
+  case 12:
+  case 13:
+  case 14:
+  case 15:
+  case 16:
+  case 17:
+  case 18:
+  case 19:
+  case 20:
+  case 21:
+  case 22:
+  case 23:
+  case 24:
+    console.log("Все таки нет правил без исключения");
+    break;
+  case 25:
+    console.log("50 на 50");
+    break;
+  default:
+    console.log("У меня 100 пудов все получится");
+};
