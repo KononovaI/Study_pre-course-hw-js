cc1dce553690a6072b04a4d70a611244e59ed172
Inga Kononova <inga.kononova@gmail.com>
2024-04-10 17:32:18 +0300

    Code for the 7th task (3rd week)

---
+++ lesson-03/07.js
@@ -0,0 +1,14 @@
+const numbers = [
+	[1, 2, 3, 4, 5],
+	[11, 20, 33, 40, 55],
+	[111, 200, 333, 400, 555],
+];
+numbers.forEach(array => {
+array.forEach((number, index) => {
+	if(number % 2 !== 0) {
+		 array.splice(index,1);
+		 }
+})
+})
+
+console.log(numbers);
